0adef9c50e0c4a037abfc169829148b5adf2b3ba
Phoebe <phoebe.manning8@gmail.com>
2018-02-12 16:32:16 -0800

    Created Words class, valid entry method, split word method, dash method, and match_and_replace method

---
+++ word_guess.rb
@@ -0,0 +1,80 @@
+#categories
+simpsons = %w[Homer Marge Lisa Bart Nelson Ned Maggie]
+
+class Words
+
+  def initialize(category)
+    @category = category
+    @lives = 5
+    @word = @category.sample
+  end
+
+  def dash
+    puts "#{@word}"
+    dashes = []
+    puts "#{@word.length}"
+    length = @word.length
+    puts "The word length is: #{length}"
+    length.times do
+      dashes << " __ "
+    end
+      # print " __ "
+    return dashes
+  end
+
+  def split_word
+    letters = @word.split("")
+    print letters
+    return letters
+  end
+
+  # def guess
+
+  #   puts "Enter letter: "
+  #   until valid_letter(guess)
+  #     puts "Enter valid letter: "
+  #     guess = gets.chomp
+  #   end
+  #   return guess
+  # end
+
+  def match_and_replace(guess)
+    letter_array = split_word
+    if letter_array.include? guess
+      puts "array includes gues"
+      puts "#{letter_array.index(guess)}"
+    end
+
+  end
+
+end
+
+
+
+def valid_letter(input_guess)
+  if input_guess =~ /[a-zA-Z]/
+    return true
+  else
+    return false
+  end
+end
+
+test = Words.new(simpsons)
+# test.random_word
+puts "#{test.dash}"
+test.split_word
+puts "Enter letter: "
+guess = gets.chomp
+until valid_letter(guess)
+  puts "Enter valid letter: "
+  guess = gets.chomp
+end
+test.match_and_replace(guess)
+# i = 1
+#
+# while lives =! 0
+# puts "Guess: "
+# guess
+# counter += 1
+# puts "Guess number #{counter} is #{guess}"
+# end
